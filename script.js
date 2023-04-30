a5f313ce9ff59ee00f54d126c23a6d2dab1d369e
Rajput-vinay <105452265+Rajput-vinay@users.noreply.github.com>
2023-04-30 15:49:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,4 +10,58 @@
         {
             MenuItems.style.maxHeight="0px"
         }
+        
+        {
+    "id": "/subscriptions/689964e9-d072-4aae-b8d1-46f0b9f0856d/resourceGroups/final-project/providers/Microsoft.BotService/botServices/final_project_bot",
+    "name": "final_project_bot",
+    "type": "Microsoft.BotService/botServices",
+    "etag": "\"31002168-0000-0200-0000-644e3d5d0000\"",
+    "location": "global",
+    "sku": {
+        "name": "S1"
+    },
+    "kind": "azurebot",
+    "tags": {},
+    "properties": {
+        "displayName": "final_project_bot",
+        "description": null,
+        "iconUrl": "https://docs.botframework.com/static/devportal/client/images/bot-framework-default.png",
+        "endpoint": "",
+        "msaAppId": "112d3a45-5cbe-45de-8e22-729513da9d9e",
+        "msaAppTenantId": "",
+        "msaAppType": "MultiTenant",
+        "msaAppMSIResourceId": "",
+        "developerAppInsightKey": null,
+        "developerAppInsightsApplicationId": null,
+        "luisAppIds": [],
+        "endpointVersion": "3.0",
+        "configuredChannels": [
+            "webchat",
+            "directline"
+        ],
+        "enabledChannels": [
+            "webchat",
+            "directline"
+        ],
+        "isDeveloperAppInsightsApiKeySet": false,
+        "isStreamingSupported": false,
+        "schemaTransformationVersion": "1.3",
+        "publishingCredentials": null,
+        "parameters": null,
+        "allSettings": null,
+        "manifestUrl": null,
+        "storageResourceId": null,
+        "tenantId": "7cde9d5d-44c0-418b-b415-1eb786077241",
+        "migrationToken": null,
+        "isCmekEnabled": false,
+        "cmekKeyVaultUrl": null,
+        "openWithHint": "",
+        "appPasswordHint": "",
+        "publicNetworkAccess": null,
+        "disableLocalAuth": false,
+        "cmekEncryptionStatus": "Off",
+        "provisioningState": "Succeeded"
+    },
+    "zones": []
+}
     }
